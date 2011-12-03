7a7f82e64b25b42dec53ce5805fdca490f9a7634
Rocky Duan <dementrock@gmail.com>
2011-12-02 17:28:29 -0800

    Updated monitors

--- course_monitor.py
+++ course_monitor.py
@@ -11,14 +11,24 @@ from coursemonitor.models import CourseMonitor
 
 SLEEP_INTERVAL = 300 
 
+LOG_FILE = 'log_course_monitor'
+
+def log(s):
+    if LOG_FILE:
+        log_file = open(LOG_FILE, 'a')
+        log_file.write(s + '\n')
+        log_file.close()
+    else:
+        print s
+
 while True:
     try:
-        print "Fetch all again..."
+        log("Fetch all again...")
         course_monitor_list = CourseMonitor.objects.all()
 
         for course in course_monitor_list:
             course.fetch()
-        print "Sleep for %d second(s)..." % SLEEP_INTERVAL
+        log("Sleep for %d second(s)..." % SLEEP_INTERVAL)
         time.sleep(SLEEP_INTERVAL)
     except Exception as e:
         errorlog(e)
--- course_page_monitor.py
+++ course_page_monitor.py
@@ -11,14 +11,26 @@ from coursepagemonitor.models import CoursePageMonitor
 
 SLEEP_INTERVAL = 300 
 
+
+LOG_FILE = 'log_course_page_monitor'
+
+def log(s):
+    if LOG_FILE:
+        log_file = open(LOG_FILE, 'a')
+        log_file.write(s + '\n')
+        log_file.close()
+    else:
+        print s
+
+
 while True:
     try:
-        print "Fetch all again..."
+        log("Fetch all again...")
         course_page_monitor_list = CoursePageMonitor.objects.all()
 
         for course_page in course_page_monitor_list:
             course_page.fetch()
-        print "Sleep for %d second(s)..." % SLEEP_INTERVAL
+        log("Sleep for %d second(s)..." % SLEEP_INTERVAL)
         time.sleep(SLEEP_INTERVAL)
     except Exception as e:
         errorlog(e)
